469e357d432e13820b7132431a1d6edc5ae4c4d7
MinhVu <minhvulai06@gmail.com>
2024-07-06 22:15:31 +0300

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Minh Vu Lai on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
