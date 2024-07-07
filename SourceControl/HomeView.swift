67b4562363f5ebe1743387d1a9b73e022369eb95
MinhVu <minhvulai06@gmail.com>
2024-07-07 10:15:11 +0300

    Add a Home View to the project

---
+++ SourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Minh Vu Lai on 7/7/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HOME")
+    }
+}
+
+#Preview {
+    HomeView()
+}
